8ce2196d9f149750ba7c9069c123cf2a45a55ad5
Anthony Rumore <anthony.rumore@ufl.edu>
2022-10-31 20:22:17 -0400

    Adds two numbers together

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    # adds
+    print("Add 2 numbers below.")
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
+    print("Result:", num1 + num2)
